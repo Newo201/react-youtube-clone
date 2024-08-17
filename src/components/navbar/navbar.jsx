2a50ba6cc8c9fc5ac0c581c6c5df74eb33e45803
newo200 <newo200coding@gmail.com>
2024-08-17 14:14:33 +1000

    Initial nav layout and main css styling

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import menu_icon from "../../assets/menu.png"
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search.png'
+import upload_icon from '../../assets/upload.png'
+import more_icon from '../../assets/more.png'
+import notifaction_icon from '../../assets/notification.png'
+import profile_icon from '../../assets/user_profile.jpg'
 
-const navbar = () => {
+export const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className = 'flex-div'>
+        {/* The left component of the navbar */}
+        <div className = 'nav-left flex-div'>
+            <img className = 'menu-icon' src = {menu_icon} alt = "" />
+            <img className = 'logo' src  = {logo} alt = ""/>
+        </div>
+        {/* The middle component of the navbar which includes the search icon*/}
+        <div className = "nav-middle flex-div">
+            <input type = "text" placeholder = "Search"/>
+        </div>
+        {/* The right component of the navbar which includes all of the icons*/}
+        <div className = "nav-middle flex-div">
+            <img className = 'upload_icon' src = {upload_icon} alt = "" />
+            <img className = 'more-icon' src = {more_icon} alt = "" />
+            <img className = 'notification-icon' src = {notifaction_icon} alt = "" />
+            <img className = 'profile-icon' src = {profile_icon} alt = "" />
+        </div>
+    </nav>
   )
 }
-
-export default navbar
