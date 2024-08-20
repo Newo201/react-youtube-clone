8e86e2ef45ded2b6c457f489b92e1426790de1f0
newo200 <newo200coding@gmail.com>
2024-08-20 11:28:10 +1000

    Display dynamic data on feed page

--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -1,29 +1,42 @@
 import Video from "./video";
+import { API_KEY } from "../../Data";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios"
 import "./feed.css"
 
-export default function Feed({sidebar}) {
+export default function Feed({sidebar, currentCategory}) {
+
+  const [data, setData] = useState([])
+
+  const fetchData = async() => {
+    const videoList_url =  'https://www.googleapis.com/youtube/v3/videos'
+    const videoList_params = {
+      'part': 'snippet, contentDetails, statistics', 'chart': 'mostPopular', 
+      'maxResults': 50, 'videoCategoryId': currentCategory, 'key': API_KEY
+    }
+    // Return the data 
+    const response = await axios.get(videoList_url, {params : videoList_params})
+    const data = response.data
+    console.log(data.items)
+    setData(data.items)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [currentCategory])
+
   return (
     <div className = {`video-grid ${sidebar?"":"large-container"}`}>
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+      {data.map((video, index) => {
+        return (
+          <Link to = {`video/${video.snippet.categoryId}/${video.id}`}>
+            <Video video_img = {video.snippet.thumbnails.medium.url} title = {video.snippet.title} 
+            channel = {video.snippet.channelTitle} views = {video.statistics.viewCount} 
+            published = {video.snippet.publishedAt}/>
+          </Link>
+        )
+      })}
     </div>
   )
 }
--- src/components/feed/video.jsx
+++ src/components/feed/video.jsx
@@ -1,16 +1,62 @@
-import video from "../../assets/thumbnail1.png"
+// import video from "../../assets/thumbnail1.png"
 import "./video.css"
+import moment from "moment"
 
-export default function Video({side}) {
+export default function Video({video_img, title, channel, views, published}) {
+
+    let displayViews
+
+    if (views >= 10**6) {
+      displayViews = `${Math.floor(views/10**6)}M`
+    }
+    else if (views >= 10**3) {
+      displayViews = `${Math.floor(views/10**3)}K`
+    }
+    else {
+      displayViews = views
+    }
+
+    const publishedDate = new Date(published)
+    const today = new Date()
+    const publishedMoment = moment(publishedDate)
+    const todayMoment = moment(today)
+
+    const [month_diff, week_diff, day_diff, hour_diff] = [
+      todayMoment.diff(published, 'months'),
+      todayMoment.diff(published, 'weeks'), 
+      todayMoment.diff(published, 'days'),
+      todayMoment.diff(published, 'hours'),
+    ]
+  
+    let displayPublished
+
+    if (hour_diff < 24) {
+      displayPublished = `${hour_diff} hours ago`
+    }
+    else if (day_diff < 7) {
+      if (day_diff === 1) {
+        displayPublished = `${day_diff} day ago`
+      }
+      else {
+        displayPublished = `${day_diff} days ago`
+      }
+    }
+    else if (week_diff < 4) {
+      displayPublished = `${week_diff} weeks ago`
+    }
+    else {
+      displayPublished = `${month_diff} months ago`
+    }
+
     return (
       <div className = 'video'>
         <div className = 'video-thumbnail'>
-            <img src = {video}/>
+            <img src = {video_img}/>
         </div>
         <div className = 'video-description'>
-            <h3>7 Days Stranded in a Cave</h3>
-            <h4>Mr Beast</h4>
-            <p>26M Views &bull; 11 Hours Ago</p>
+            <h3>{title}</h3>
+            <h4>{channel}</h4>
+            <p>{displayViews} Views &bull; {displayPublished}</p>
         </div>
       </div>
     )
